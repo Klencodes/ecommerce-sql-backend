20b2cb974a95a558aeac2aba2d591aca441a6203
unknown <klencodes@gmail.com>
2020-05-18 23:37:08 +0100

    setting up backend server

---
+++ app.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
+const app = express();
+const cors = require('cors');
+
+
+/* CORS */
+app.use(cors({
+    origin: '*',
+    methods: ['GET', 'PUT', 'DELETE', 'PATCH', 'POST'],
+    allowedHeaders: 'Content-Type, Authorization, Origin, X-Requested-With, Accept'
+}));
+app.use(logger('combined'));
+app.use(express.json());
+app.use(express.urlencoded({extended: false}));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+
+// Import Routes
+const usersRouter = require('./routes/users');
+const productsRouter = require('./routes/products');
+
+app.use('/api/users', usersRouter);
+app.use('/api/products', productsRouter);
+
+
+module.exports = app;
